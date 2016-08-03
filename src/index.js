63e17d5a1fe1e0ec33713a3067084544da334bc9
valerijpll <valerijpll@gmail.com>
2016-08-04 00:56:17 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill'
+import React from 'react'
+import Fetch from 'react-fetch'
+import { render } from 'react-dom'
+import App from './containers/App'
+
+
+render(<App />, document.getElementById('app'));
+
+
+// render(<App />,document.getElementById('root'))
